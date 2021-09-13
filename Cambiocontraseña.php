893c6df1d774e2ab5b157daea5d36c20b1a63d86
Aymer Sena <aycruz04@misena.edu.co>
2021-09-12 22:04:40 -0500

    Se corrigio la pagina de cambio de contraseña

--- Cambiocontraseña.php
+++ Cambiocontraseña.php
@@ -9,15 +9,15 @@
 </head>
 <body>
     <header id="encabezado">
-        <a href=""><img id="logo-header" src="logoPag.png"></a>
-        <img id="foto_perfil" src="descarga.png">
+        <a href=""><img id="logo-header" src="https://1.bp.blogspot.com/-CRGFBvE8s8k/YT5yRhIEj8I/AAAAAAAAAHM/dplt4qgxJmcjfSP213rWRyF0EoW_BQlSACNcBGAsYHQ/s320/logoPag.png"></a>
+        <img id="foto_perfil" src="https://1.bp.blogspot.com/-MeCxaLO8njU/YT5yRMu7KrI/AAAAAAAAAHI/NhoZIlmquMUWDoiVjAzF3nTF1WnwqRTSQCNcBGAsYHQ/s0/descarga.png">
         <nav>
             <ul>
                 <li>
-                    <a href=""><img class="logito" src="IconoNotificaciones.png"></a>
+                    <a href=""><img class="logito" src="https://1.bp.blogspot.com/-EO10WM7B0Ig/YT5yOT5S7JI/AAAAAAAAAGw/FfqaAQ19Y709UTCC9jBUt7CW9pEv8_xjACNcBGAsYHQ/s0/IconoNotificaciones.png"></a>
                 </li>
                 <li>
-                    <a href="index.html"><img class="logito" src="IconoSalir.png"></a>
+                    <a href="index.html"><img class="logito" src="https://1.bp.blogspot.com/-BM03tlw4TH0/YT5yOxEwdoI/AAAAAAAAAG0/LneMrf5vRD0ooVH6n92poAdrIa8balaRACNcBGAsYHQ/s0/IconoSalir.png"></a>
                 </li>
             </ul>
         </nav>
