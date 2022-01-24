6e1aa4d31377018d818ae15a14412698113fd2aa
Aymer Sena <aycruz04@misena.edu.co>
2022-01-24 17:28:37 -0500

    Se agregaron los diseños de el modulo informe de nomina, falta configurar tabla y grafica

--- informeNomina.php
+++ informeNomina.php
@@ -26,7 +26,34 @@
         </div>
         <div class="container">
             <div class="subcontenido">
-            <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit. Iure maxime suscipit tempora, cupiditate vel aspernatur? Commodi quibusdam hic, animi, maiores, libero corrupti officia necessitatibus temporibus labore saepe tempora quod nulla.</p>
+            <div class="secondConteiner">
+                <div class="conteinerLeft">
+               <form action="POST">
+                   <h2>Tabla de resultados  </h2>
+                   <select name="areaSelect" id="areaSelect">
+                       <option value="*">Seleccione área</option>
+                   </select>
+                   <select name="peopleSelect" id="peopleSelect">
+                       <option value="*">Selccione funcionario</option>
+                   </select>
+               </form>
+               <table class="resultTable">
+                   <tr>
+                       <th>Fecha</th>
+                       <th>Funcionario</th>
+                       <th>Salario</th>
+                   </tr>
+                   <tr>
+                       <td>Fecha Ficticia</td>
+                       <td>Funcionario ficticio</td>
+                       <td>Salario ficticio</td>
+                   </tr>
+               </table>
+               </div>
+               <div class="conteinerRight">
+                   <h2>Acá va la grafica.</h2>
+               </div>
+            </div>
             </div>
             <img src="https://1.bp.blogspot.com/-CRGFBvE8s8k/YT5yRhIEj8I/AAAAAAAAAHM/dplt4qgxJmcjfSP213rWRyF0EoW_BQlSACNcBGAsYHQ/s332/logoPag.png" id="logoUnix">
         </div>
