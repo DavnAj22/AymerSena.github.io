21e69aa2a1a2a941d19c8c93f5bce687d9f9cb16
Aymer Sena <aycruz04@misena.edu.co>
2022-01-24 20:55:32 -0500

    Se agregaron nuevos archivos para el modulo de cambiar la foto de perfil, para iniciar con las pruebas de adjuntar documentos al servidor

--- menuAdministrador.php
+++ menuAdministrador.php
@@ -14,7 +14,7 @@
         <div class="panelLeft">
             <nav>
                 <ul>
-                    <li><img src="https://1.bp.blogspot.com/-MeCxaLO8njU/YT5yRMu7KrI/AAAAAAAAAHI/NhoZIlmquMUWDoiVjAzF3nTF1WnwqRTSQCNcBGAsYHQ/s0/descarga.png" id="photoPer"></li>
+                    <li><a href="photoPerfil.php"><img src="https://1.bp.blogspot.com/-MeCxaLO8njU/YT5yRMu7KrI/AAAAAAAAAHI/NhoZIlmquMUWDoiVjAzF3nTF1WnwqRTSQCNcBGAsYHQ/s0/descarga.png" id="photoPer"></a></li>
                     <li> <img src="https://1.bp.blogspot.com/-EO10WM7B0Ig/YT5yOT5S7JI/AAAAAAAAAGw/FfqaAQ19Y709UTCC9jBUt7CW9pEv8_xjACNcBGAsYHQ/s0/IconoNotificaciones.png" id="photoNoti"></li>
                     <li><button>Gestion contraseña</button></li>
                     <li><button>Cerrar sesión</button></li>
