aea741bfeca3c9833ea3afbfb96336150e3cc526
Aymer Sena <aycruz04@misena.edu.co>
2022-02-08 20:15:10 -0500

    Se agrego de manera correcta el codigo para la subida de archivos pero esta ruta no se esta almacenando en la base de datos, ademas solo se implemento para el tema de cambiar la foto de perfil pero no se ha parametrizado de manera correcta sobre el tipo de archivo, peso y demas.

--- photoPerfil.php
+++ photoPerfil.php
@@ -26,9 +26,9 @@
         </div>
         <div class="container">
             <div class="formularioFoto">
-                <form action="" method="$_POST">
+            <form enctype="multipart/form-data" action="uploadPhoto.php" method="POST">
                     <label for="imagen">Imagen:</label>
-                    <input id="imagen" name="imagen" size="30" type="file"><br>
+                    <input id="imagen" name="subir_archivo" size="30" type="file"><br>
                     <button>Enviar</button>
                 </form>
             </div>
@@ -40,62 +40,5 @@
             <h2>Pie de página va acá</h2>
         </footer>
     </div>
-<?php
-
-require("conexionBD.php");
-
-// Recibo los datos de la imagen
-$nombre_img = $_FILES['imagen']['name'];
-$tipo = $_FILES['imagen']['type'];
-$tamano = $_FILES['imagen']['size'];
-
-//Si existe imagen y tiene un tamaño correcto
-if (!empty($nombreImg) && ($_FILES['imagen']['size'] <= 200000)) 
-{
-   //indicamos los formatos que permitimos subir a nuestro servidor
-   if (($_FILES["imagen"]["type"] == "image/gif")
-   || ($_FILES["imagen"]["type"] == "image/jpeg")
-   || ($_FILES["imagen"]["type"] == "image/jpg")
-   || ($_FILES["imagen"]["type"] == "image/png"))
-   {
-      // Ruta donde se guardarán las imágenes que subamos
-      $directorio = $_SERVER['DOCUMENT_ROOT'].'/Imagenes/';
-      // Muevo la imagen desde el directorio temporal a nuestra ruta indicada anteriormente
-      move_uploaded_file($_FILES['imagen']['tmp_name'],$directorio.$nombre_img);
-    } 
-    else 
-    {
-       //si no cumple con el formato
-       echo "No se puede subir una imagen con ese formato ";
-    }
-} 
-else 
-{
-   //si existe la variable pero se pasa del tamaño permitido
-   if($nombre_img == !NULL) echo "La imagen es demasiado grande "; 
-}
-
-/* en pasos anteriores deberíamos tener una conexión abierta a nuestra base de 
-datos para ejecutar nuestra sentencia SQL */
-
-/* con la siguiente sentencia le asignamos a nuestro campo de la tabla ruta_imagen 
-el nombre de nuestra imagen */
-
-$sql = "UPDATE tblusuario SET UsuFotoPerfil = '$nombre_img' ";
-$result = $conexion->query($sql);
-
-/* volvemos a la página principal para cargar la imagen que hemos subido */
-header("Location: paginaperfil.php");
-
-/* lanzamos la consulta para traernos el nombre de la imagen, en nuestro caso 
-el campo ruta_imagen se encuentra en la tabla usuarios */ 
-$result2 = $conexion->query("SELECT * FROM usuarios "); 
-while ($row=$conexion->mysql_fetch_array($result2))
-{ 
-    /*almacenamos el nombre de la ruta en la variable $ruta_img*/ 
-    $ruta_img = $row["ruta_imagen"]; 
-} 
-
-?>
 </body>
 </html>
